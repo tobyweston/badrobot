a32b3f0440bd4b0610fb7601132d6cbd6b5ac828
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2010-06-09 20:35:30 +0000

    updated with thoughts on Repository, clarified what an AggregateRoot is. Thinking (at the mo) in terms of CRUD for the repository, but may describe it (at least by interface name) less in terms of CRUD (implies database) and more collection semantics. The only reason the Repostory has no methods and extended interfaces do, is that not all repository implementations will need them and I'm expecting implmenting classes to provide more specific methods. For example, a Repository that needs to implement two findBy methods (which java barfs at with its erasure crap)

--- bad.robot/ddd/src/main/java/bad/robot/ddd/Aggregate.java
+++ bad.robot/ddd/src/main/java/bad/robot/ddd/Aggregate.java
@@ -20,15 +20,26 @@ package bad.robot.ddd;
  * A cluster of associated objects ({@link bad.robot.ddd.Entity} or {@link bad.robot.ddd.ValueObject}) that are treated
  * as a unit for the purpose of model changes. Only a single member should be referenced externally, this is
  * the {@link bad.robot.ddd.AggregateRoot}.
+ * <p/>
+ * Because an {@link bad.robot.ddd.Aggregate} captures some domain noun, it should be made up of other well formed
+ * domain constructs. It should therefore only contain members which are of the following types.
+ * <ul>
+ * <li>{@link bad.robot.ddd.ValueObject}</li>
+ * <li>{@link Entity}</li>
+ * <li>{@link Aggregate}</li>
+ * </ul>
+ * If it were to be made up of for example, vanilla objects, the <i>meaning</i> of that object within the business context
+ * would be lost and the value of the {@link bad.robot.ddd.Aggregate} is reduced. How can the team have a clear understanding
+ * of this {@link bad.robot.ddd.Aggregate} if it is composed of less well understood composites.
  *
- * @see {@link BoundedContext}
- * @see {@link bad.robot.ddd.AggregateRoot}
- * @see {@link bad.robot.ddd.Entity}
- * @see {@link bad.robot.ddd.ValueObject}
+ * @see bad.robot.ddd.BoundedContext
+ * @see bad.robot.ddd.AggregateRoot
+ * @see bad.robot.ddd.Entity
+ * @see bad.robot.ddd.ValueObject
  */
 public @interface Aggregate {
 
-    // members should be Entity or ValueObjects.
-
+    // not clear about the relationship between aggregate and entity. is it a "is a"? maybe not as it can still be a valid
+    // domain aggregate without an id?
 
 }
