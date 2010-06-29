8d9a0c51c400439c55a8b4691b510a8e3bb498f9
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2010-06-29 20:45:26 +0000

    moved things around some more, trying out a self documenting approach to the user manual...

---
+++ bad.robot/concordion-ant-task/src/manual/java/Overview.java
@@ -0,0 +1,8 @@
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class Overview {
+}
