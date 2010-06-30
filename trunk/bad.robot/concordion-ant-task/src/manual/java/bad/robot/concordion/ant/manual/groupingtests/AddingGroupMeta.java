ac67c1b35606ed83131164df71aaef9a6d2dbf3f
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2010-06-30 08:38:06 +0000

    added more docs

---
+++ trunk/bad.robot/concordion-ant-task/src/manual/java/bad/robot/concordion/ant/manual/groupingtests/AddingGroupMeta.java
@@ -0,0 +1,10 @@
+package bad.robot.concordion.ant.manual.groupingtests;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class AddingGroupMeta {
+}
