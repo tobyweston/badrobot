60eddf197294d7e676b77b7b7204cee6f0cf0276
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2010-07-07 07:34:15 +0000

    more tests

---
+++ trunk/bad.robot/concordion-ant-task/src/manual/java/bad/robot/concordion/ant/manual/groupingtests/AddingMultipleGroupMeta.java
@@ -0,0 +1,24 @@
+package bad.robot.concordion.ant.manual.groupingtests;
+
+import org.concordion.api.ExpectedToPass;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
+@RunWith(ConcordionRunner.class)
+@ExpectedToPass
+public class AddingMultipleGroupMeta {
+
+    private final GenerateTestOverviewFixture fixture = new GenerateTestOverviewFixture();
+
+    public void generateOverview(String page) throws IOException {
+        fixture.generateOverviewFullPage(page);
+    }
+
+    public String getOverview() throws IOException, TransformerException {
+        return fixture.getOverviewGroupSection();
+    }
+
+}
