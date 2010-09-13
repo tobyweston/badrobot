c6bca050f29aa43ca0f3e5f87bf26d2507f8fb22
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2010-09-13 20:20:05 +0000

    started test for Bug #1

--- bad.robot/concordion-ant-task/src/manual/java/bad/robot/concordion/ant/manual/groupingtests/TaskFactory.java
+++ bad.robot/concordion-ant-task/src/manual/java/bad/robot/concordion/ant/manual/groupingtests/TaskFactory.java
@@ -7,28 +7,48 @@ import org.apache.tools.ant.types.FileSet;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static bad.robot.concordion.ant.FileUtils.*;
 import static java.lang.String.format;
 
 public class TaskFactory {
 
-    public static GenerateTestOverviewTask createTask(String page, String template) throws IOException {
+    public static GenerateTestOverviewTask createTask(String template, String... pages) throws IOException {
         GenerateTestOverviewTask task = new GenerateTestOverviewTask();
         File output = createTemporaryFile(workingFolder());
         task.setOutput(output);
         task.setTemplate(first(findRecursively(workingFolder(), matching(template))));
-        task.add(createFileSet(page, first(findRecursively(workingFolder(), matching(page)))));
+        task.add(createFileSet(pages));
         return task;
     }
 
-    private static FileSet createFileSet(String page, File file) {
+    private static FileSet createFileSet(String... pages) {
         FileSet fileSet = new FileSet();
-        fileSet.setDir(file.getParentFile());
-        fileSet.setIncludes(page);
+        File folder = getParentFolder(pages);
+        fileSet.setDir(folder);
+        fileSet.setIncludes(concatenationOf(pages));
         fileSet.setProject(new Project());
         return fileSet;
+    }
+
+    private static String concatenationOf(String... pages) {
+        StringBuilder builder = new StringBuilder();
+        for (int current = 0; current < pages.length; current++)
+            builder.append(pages[current]).append(",");
+        return builder.toString();
+    }
+
+    private static File getParentFolder(String[] pages) {
+        Set<File> files = new HashSet<File>();
+        for (String page : pages)
+            files.add(first(findRecursively(workingFolder(), matching(page))));
+        if (files.size() != 1)
+            throw new RuntimeException("incorrect number of folders found, you can only test Concordion specfications from the same folder");
+        return files.toArray(new File[]{})[0].getParentFile();
+
     }
 
     private static FilenameFilter matching(final String page) {
@@ -44,5 +64,5 @@ public class TaskFactory {
             throw new IllegalStateException(format("couldn't find file looking recursively from %s", workingFolder().getAbsolutePath()));
         return files.get(0);
     }
-    
+
 }
