0601248069d51982a70d8cd5003092ed9b434010
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2011-02-10 08:58:03 +0000

    added test for thread factory

---
+++ trunk/bad.robot/conconc/src/test/java/bad/robot/pingpong/instrument/InstrumentingThreadFactoryTest.java
@@ -0,0 +1,76 @@
+package bad.robot.pingpong.instrument;
+
+import bad.robot.pingpong.memory.shared.ThreadStatistics;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JMock.class)
+public class InstrumentingThreadFactoryTest {
+
+    private final Mockery context = new Mockery();
+    private final ThreadStatistics statistics = context.mock(ThreadStatistics.class);
+    private final InstrumentingThreadFactory factory = new InstrumentingThreadFactory(statistics);
+
+    @Test
+    public void willDelegateToRunnable() {
+        final Runnable runnable = context.mock(Runnable.class);
+        context.checking(new Expectations(){{
+            one(runnable).run();
+            ignoring(statistics);
+        }});
+        factory.newThread(runnable).run();
+    }
+    
+    @Test
+    public void willIncrementActiveThreadCountWhenStarted() {
+        context.checking(new Expectations(){{
+            one(statistics).incrementActiveThreads();
+            ignoring(statistics).decrementActiveThreads();
+            ignoring(statistics).incrementThreadCount();
+        }});
+        factory.newThread(stub()).run();
+    }
+
+    @Test
+    public void willDecrementActiveThreadCountWhenDone() {
+        context.checking(new Expectations(){{
+            ignoring(statistics).incrementActiveThreads();
+            one(statistics).decrementActiveThreads();
+            ignoring(statistics).incrementThreadCount();
+        }});
+        factory.newThread(stub()).run();
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void willDecrementActiveThreadCountWhenExceptionOccurs() {
+        final Runnable runnable = context.mock(Runnable.class);
+        context.checking(new Expectations() {{
+            one(runnable).run(); will(throwException(new RuntimeException()));
+            ignoring(statistics).incrementActiveThreads();
+            one(statistics).decrementActiveThreads();
+            ignoring(statistics).incrementThreadCount();
+        }});
+        factory.newThread(runnable).run();
+    }
+
+    @Test
+    public void willIncrementThreadCount() {
+        context.checking(new Expectations(){{
+            never(statistics).incrementActiveThreads();
+            never(statistics).decrementActiveThreads();
+            one(statistics).incrementThreadCount();
+        }});
+        factory.newThread(stub());
+    }
+
+    private static Runnable stub() {
+        return new Runnable() {
+            @Override
+            public void run() {
+            }
+        };
+    }
+}
