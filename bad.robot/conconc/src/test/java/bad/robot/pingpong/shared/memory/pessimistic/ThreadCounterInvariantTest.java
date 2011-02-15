3c0f54cff976099fe32020e68761964b8e36adc1
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2011-02-15 15:56:26 +0000

    added Guard interface to decouple the locking/synchronizing policy from the ThreadCounter, mainly motivated by trying to test the invariant when resetting the counters (that it should be done atomically).

---
+++ bad.robot/conconc/src/test/java/bad/robot/pingpong/shared/memory/pessimistic/ThreadCounterInvariantTest.java
@@ -0,0 +1,49 @@
+package bad.robot.pingpong.shared.memory.pessimistic;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+@RunWith(JMock.class)
+public class ThreadCounterInvariantTest {
+
+    private final Mockery context = new Mockery();
+    private final Lock lock = context.mock(Lock.class);
+    private final ThreadCounter counter = new ThreadCounter(new LockingGuard(lock));
+
+    @Test
+    public void shouldLockOnWrites() throws Exception {
+        context.checking(new Expectations() {{
+            exactly(3).of(lock).lock();
+            exactly(3).of(lock).unlock();
+        }});
+        counter.threadCreated();
+        counter.threadStarted();
+        counter.threadTerminated();
+    }
+
+    @Test
+    public void shouldLockForResetAndSoMaintainInvariant() throws InterruptedException {
+        context.checking(new Expectations() {{
+            one(lock).tryLock(with(any(Long.class)), with(any(TimeUnit.class))); will(returnValue(true));
+            one(lock).lock();
+            one(lock).unlock();
+        }});
+        counter.reset();
+    }
+
+    @Test
+    public void shouldNotLockOnReads() {
+        context.checking(new Expectations(){{
+            never(lock);
+        }});
+        counter.getCreatedCount();
+        counter.getActiveCount();
+    }
+
+}
--- bad.robot/conconc/src/test/java/bad/robot/pingpong/shared/memory/pessimistic/ThreadCounterTest.java
+++ bad.robot/conconc/src/test/java/bad/robot/pingpong/shared/memory/pessimistic/ThreadCounterTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertThat;
 
 public class ThreadCounterTest {
 
-    private final ThreadCounter counter = new ThreadCounter();
+    private final ThreadCounter counter = new ThreadCounter(new Unguarded());
 
     @Test
     public void shouldInitialiseCounts() {
