e5649551c40982404a825bead38c95930c743e03
toby.weston@gmail.com <toby.weston@gmail.com@6e19889a-6cb1-5c66-3653-dff68049a833>
2011-12-06 12:50:54 +0000

    updating user manual to include download instructions

---
+++ trunk/bad.robot/concordion-ant-task/src/manual/java/bad/robot/concordion/ant/manual/Download.java
@@ -0,0 +1,11 @@
+package bad.robot.concordion.ant.manual;
+
+import org.concordion.api.Unimplemented;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+@Unimplemented
+public class Download {
+
+}
